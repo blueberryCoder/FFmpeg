d876ef1f3adcf4185eeeedd02596252dbeb66566
muyonggang <muyonggang@bytedance.com>
2023-03-09 08:08:35 +0800

    add config_pc for mac

---
+++ config_pc.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+./configure \
+--enable-gpl \
+--enable-filter=aresample \
+--enable-bsf=aac_adtstoasc \
+--enable-small \
+--enable-dwt \
+--enable-lsp \
+--enable-mdct \
+--enable-rdft \
+--enable-fft \
+--enable-static \
+--enable-version3 \
+--enable-nonfree \
+--enable-encoder=libfdk_aac \
+--enable-encoder=libx264 \
+--enable-decoder=mp3 \
+--enable-libx264 \
+--enable-libfdk_aac \
+--enable-libmp3lame \
+--enable-debug=3 \
+--disable-optimizations \
+--disable-asm \
+--disable-stripping \
+--enable-ffplay \
+--enable-libfreetype \
+--enable-libfontconfig \
+--enable-libfribidi \
+--extra-cflags="-I/usr/local/include" \
+--extra-ldflags="-L/usr/local/lib" 
